23de9e93d7919a5b52bb54280b55728bf78e1a6e
Volgin7 <8612020@gmail.com>
2023-02-22 12:57:06 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+
+
 }
