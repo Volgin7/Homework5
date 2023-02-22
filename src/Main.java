b3ce38c2c55df58e2ab147dbbdbb55e4dbf2ffab
Volgin7 <8612020@gmail.com>
2023-02-22 13:05:24 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
     }
 
     public static void task1() {
@@ -14,5 +14,21 @@ public class Main {
         }
     }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear < 2015 ) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientDeviceYear < 2015 ) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+    }
 
 }
